c35beda884a3e95903d12348a41f51de8650fb8c
noah lahaise <noah.lahaise1@gmail.com>
2023-03-29 00:30:14 -0400

    Adding NASDAQ changes, TODO in file explains fix needed

--- DatasetCleaningTemp/Dataset_Cleaning.py
+++ DatasetCleaningTemp/Dataset_Cleaning.py
@@ -8,14 +8,16 @@ import glob
 from pathlib import Path
 
 def addingToDataFrame(df):
-  path = r'C:\Users\noahl\OneDrive\Desktop\Data Analytics\DatasetCleaningTemp\Datasets\*.csv'
+  path = r'C:\DataAnalytics\DataAnalytics\DatasetCleaningTemp\Datasets\*.csv'
 
   for fname in glob.glob(path): 
 
-    if fname == r'C:\Users\noahl\OneDrive\Desktop\Data Analytics\DatasetCleaningTemp\Datasets\CPI.csv':
+    #ignore CPI was loaded before func call
+    if fname == r'C:\DataAnalytics\DataAnalytics\DatasetCleaningTemp\Datasets\CPI.csv':
        continue
     
-    elif fname == r'C:\Users\noahl\OneDrive\Desktop\Data Analytics\DatasetCleaningTemp\Datasets\S&P 500 Historical Data.csv':
+    #read in and edit S&P500 
+    elif fname == r'C:\DataAnalytics\DataAnalytics\DatasetCleaningTemp\Datasets\S&P 500 Historical Data.csv':
       file = pd.read_csv(fname)
       
       file.drop('Open', inplace=True, axis=1)
@@ -27,10 +29,18 @@ def addingToDataFrame(df):
       file = SPY_Cleaning(file)
 
       file.drop('Date', inplace=True, axis=1)
-      #print(file)
       df["S&P500"] = file.iloc[:,0].values
       print(df)
-      
+    
+    elif fname == r'C:\DataAnalytics\DataAnalytics\DatasetCleaningTemp\Datasets\NASDAQCOM.csv':
+      file = pd.read_csv(fname)
+      file = NASDAQ_Cleaning(file)
+      file.drop('DATE', inplace=True, axis=1)
+
+      #print(file.iloc[:,0].values)
+      #df["NASDAQ"] = file.iloc[:,0].values  #TODO does not like when I try to append nasdaq same way I did S&P above
+      #TODO nasdaq has lest datapoints than it is supposed to must investigate
+    
     else:
       file = pd.read_csv(fname)
       file.drop('DATE', inplace=True, axis=1)
@@ -40,9 +50,15 @@ def addingToDataFrame(df):
   
   return df
 
-def NASDAQ_Cleaning():
-  print('clean nasdaq dataset')
-  # by cleaning this just means making the data quarterly and not daily
+def NASDAQ_Cleaning(dataSet):
+  for index, row in dataSet.iterrows():
+       
+      if('01/01' not in row['DATE'] and '04/01' not in row['DATE'] 
+         and '07/01' not in row['DATE'] and '12/01' not in row['DATE']):
+          
+          dataSet = dataSet.drop(labels=index, axis=0)
+      
+  return dataSet
 
 
 def SPY_Cleaning(dataSet):
@@ -55,10 +71,7 @@ def SPY_Cleaning(dataSet):
       
     return dataSet
 
-#fedfunds = pd.read_csv(r'C:\Users\raghu\Documents\Education\College\8 23 Spring Sem\CAP4784\Project\Datasets\FEDFUNDS.csv')
-#print(fedfunds)
-
-dataframe = pd.read_csv(r'C:\Users\noahl\OneDrive\Desktop\Data Analytics\DatasetCleaningTemp\Datasets\CPI.csv')
+dataframe = pd.read_csv(r'C:\DataAnalytics\DataAnalytics\DatasetCleaningTemp\Datasets\CPI.csv')
 
 dataframe = addingToDataFrame(dataframe)
 dataframe = dataframe.set_index('DATE')
